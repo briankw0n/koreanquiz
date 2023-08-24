7cbeb6dec491149c4cc88d642a6c3097e20d03d9
Brian Kwon <113305772+briankw0n@users.noreply.github.com>
2023-08-24 11:13:41 -0700

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://ko.wiktionary.org/wiki/%EB%B6%80%EB%A1%9D:%ED%95%9C%EB%AC%B8_%EA%B5%90%EC%9C%A1%EC%9A%A9_%EA%B8%B0%EC%B4%88_%ED%95%9C%EC%9E%90_1800#%E3%85%8D"
+
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    td_elements = soup.find_all("td")[4:]
+    
+    with open("extracted_data.txt", "w", encoding="utf-8") as f:
+        for td in td_elements:
+            extracted_text = ''
+
+            for content in td.contents:
+                if content.name == 'a':
+                    extracted_text += '{\n\tquestion: \'' + content.get_text(strip=True) + '\',\n\tanswer: \''
+                elif content.name == 'small':
+                    small_text = content.get_text(strip=True)
+                    #extracted_text += small_text[:1]
+                    
+                    a_tags = content.find_all("a")
+                    for a_tag in a_tags:
+                        extracted_text += a_tag.get_text(strip=True) + ' '
+                        a_len = len(a_tag.get_text(strip=True))
+
+                    extracted_text += small_text[a_len + 1:-1] + '\'\n},\n'
+            if extracted_text:
+                f.write("{}".format(extracted_text))
+    print("Scrape complete.")
+else:
+    print("Failed to retrieve the page.")
---
+++ script.js
@@ -0,0 +1,41 @@
+import { hanjaCharacters } from './characters.js';
+
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const next = document.getElementById("next");
+let card = document.getElementById("card");
+let charList = hanjaCharacters;
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function initializeCard() {
+  const cardFront = document.querySelector(".card-front");
+  const cardBack = document.querySelector(".card-back");
+
+  charList = shuffleArray(hanjaCharacters);
+  const currentCard = charList[0];
+  cardFront.textContent = currentCard.question;
+  cardBack.textContent = currentCard.answer;
+}
+initializeCard();
+
+function flipCard() {
+  card.style.transition = "transform 0.5s";
+  card.classList.toggle("flipped");
+}
+
+function nextCard() {
+  card.style.transition = "transform 0s";
+  card.classList.remove("flipped");
+  initializeCard();
+}
+
+question.addEventListener("click", flipCard);
+answer.addEventListener("click", flipCard);
+next.addEventListener("click", nextCard);
